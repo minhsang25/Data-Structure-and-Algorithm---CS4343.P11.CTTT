b630397ceb9119c575105da075a0741fe6a79824
Minh Sang Le <connectic2508@gmail.com>
2024-09-27 07:55:40 +0700

    Fix code(q1, q2)
    - Fix code q1, q2

--- Lab_1_Class_work.cpp
+++ Lab_1_Class_work.cpp
@@ -1,1 +1,185 @@
+//ID: 22521240
+//FULL NAME: Le Minh Sang
+//In-Class Exercise - Part 1
+
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* pnext;
+};
+
+class LinkedList {
+    public:
+    Node* phead;
+    Node* ptail;
+    
+    LinkedList () {
+        phead = ptail = nullptr;
+    }
+
+    // Node* CreateNode(int info) {
+    //     Node* temp = new Node;
+    //     temp->data = info;
+    //     temp->pnext = nullptr;
+    //     return temp;
+    // }
+
+    void AddNode(int info) {
+        Node* temp = CreateNode(info);
+        if (phead == nullptr) {
+            phead = ptail = temp;
+        } else {
+            ptail->pnext = temp; 
+            ptail = temp;
+        }
+    }
+    void PrintList(){
+        Node* temp = phead;
+        if(phead == nullptr)
+        {
+            cout<<"Empty list";
+        }
+        else 
+        {
+            while(temp != nullptr)
+            {   
+                cout<<temp->data<<" ";
+                temp = temp->pnext;
+            }
+        }
+    }
+    int CountElm (){
+        int cnt =0;
+        Node* temp = phead;
+        while(temp != nullptr)
+        {   
+            temp = temp->pnext;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    int FindMiddleNode (){
+        Node* temp = phead;
+        int length = CountElm();
+        int mid = length/2;
+        int cnt = 0;
+        while(temp != nullptr)
+        {   
+            if(length %2 !=0){
+                if(cnt = mid){
+                    cout<<temp->data<<" ";
+                }
+                else{
+                    temp = temp->pnext; 
+                    cnt++;
+                }
+            }
+            else 
+            {
+                if(cnt = mid - 1||cnt = mid){
+                    cout<<temp->data<<" ";
+                }
+                else{
+                    temp = temp->pnext; 
+                    cnt++;
+                }
+            }
+        } 
+    }
+
+    void DetectCycle (){
+        Node* slow = phead;
+        Node* fast = phead;
+        if(slow == fast){
+            cout<<"This list is cycle";
+        }
+        else {
+            cout<<"This list isn't cycle";
+        }
+    }
+
+    void ReverseList() {
+        Node* prev = nullptr;
+        Node* curr = phead;
+        Node* next = nullptr;
+        while(curr != nullprt){
+            next = curr->pnext;
+            curr->pnext = prev;
+            prev = curr;
+            curr = next;
+        }
+        phead = prev;
+        cout<<"Reversed list: ";
+        PrintList();
+    }
+
+    void EliminateDuplicates() {
+        if (phead == nullptr) return;
+        Node* curr = phead;
+        while(curr != nullptr && curr->pnext != nullptr){
+            if(curr->data == curr->pnext->data){
+                Node* temp = curr->pnext;
+                curr->pnext = curr->pnext->pnext;
+                delete temp;
+            }
+            else{
+                curr = curr->pnext;
+            }
+        }
+        cout<<"List after removing duplicates: ";
+        PrintList();
+    }
+
+//chưa có câu 7
+
+    void Search(int x){
+        Node* temp = phead;
+        while(temp != nullptr){
+            if(temp->data == x){
+                cout<<"Node with value "<<x<<" found at address: "<<temp<<endl;
+                return temp;
+            }
+        }
+        cout<<"Node with value "<<x<<" not found."<<end;
+        return nullptr;    
+    }
+};
+
+void Menu (){
+    LinkedList list;
+    int choice, value;
+    do{
+        cout<<"1. Find the Middle Node of a Linked List\n";
+        cout<<"2. Detect the cycle in a linked list\n";
+        cout<<"3. Combine Two Sorted Linked Lists\n";
+        cout<<"4. Find the Intersection of Two Linked Lists\n";
+        cout<<"5. Reverse a Linked List\n";
+        cout<<"6. Eliminate Duplicates from a Sorted Linked List\n";
+        cout<<"7. Check if a Linked List is a Palindrome\n";
+        cout<<"8. Search node\n";
+        cout<<"9. Exist.\n";
+        switch (choice){
+            case 1:
+            
+        }
+
+    }
+}
+
+int main() {
+    LinkedList list;
+
+    
+    Node* current = list.phead;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->pnext;
+    }
+    system("pause");
+    return 0;
+}
 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* pnext;
+    Node(int info) : data(info), pnext(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* phead;
+    Node* ptail;
+
+    LinkedList() {
+        phead = ptail = nullptr;
+    }
+
+    Node* CreateNode(int info) {
+        return new Node(info);
+    }
+
+    void AddNode(int info) {
+        Node* temp = CreateNode(info);
+        if (phead == nullptr) {
+            phead = ptail = temp;
+        } else {
+            ptail->pnext = temp;
+            ptail = temp;
+        }
+    }
+
+    void PrintList() {
+        Node* temp = phead;
+        if (phead == nullptr) {
+            cout << "Empty list" << endl;
+        } else {
+            while (temp != nullptr) {
+                cout << temp->data << " ";
+                temp = temp->pnext;
+            }
+            cout << endl;
+        }
+    }
+
+    int CountElm() {
+        int cnt = 0;
+        Node* temp = phead;
+        while (temp != nullptr) {
+            temp = temp->pnext;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    void FindMiddleNode() {
+        int length = CountElm();
+        int mid = length / 2;
+        Node* temp = phead;
+        int cnt = 0;
+        while (temp != nullptr) {
+            if (cnt == mid) {
+                cout << "Middle node: " << temp->data << endl;
+                break;
+            }
+            temp = temp->pnext;
+            cnt++;
+        }
+    }
+
+    void DetectCycle() {
+        Node* slow = phead;
+        Node* fast = phead;
+        while (fast != nullptr && fast->pnext != nullptr) {
+            slow = slow->pnext;
+            fast = fast->pnext->pnext;
+            if (slow == fast) {
+                cout << "This list is circular." << endl;
+                return;
+            }
+        }
+        cout << "This list isn't circular." << endl;
+    }
+
+    void ReverseList() {
+        Node* prev = nullptr;
+        Node* curr = phead;
+        Node* next = nullptr;
+        while (curr != nullptr) {
+            next = curr->pnext;
+            curr->pnext = prev;
+            prev = curr;
+            curr = next;
+        }
+        phead = prev;
+        cout << "Reversed list: ";
+        PrintList();
+    }
+
+    void EliminateDuplicates() {
+        if (phead == nullptr) return;
+        Node* curr = phead;
+        while (curr != nullptr && curr->pnext != nullptr) {
+            if (curr->data == curr->pnext->data) {
+                Node* temp = curr->pnext;
+                curr->pnext = curr->pnext->pnext;
+                delete temp;
+            } else {
+                curr = curr->pnext;
+            }
+        }
+        cout << "List after removing duplicates: ";
+        PrintList();
+    }
+
+    bool IsPalindrome() {
+        if (phead == nullptr || phead->pnext == nullptr) return true;
+
+        // Find the middle of the list
+        Node* slow = phead;
+        Node* fast = phead;
+        Node* prev_of_slow = nullptr;
+
+        while (fast != nullptr && fast->pnext != nullptr) {
+            fast = fast->pnext->pnext;
+            prev_of_slow = slow;
+            slow = slow->pnext;
+        }
+
+        Node* second_half = slow;
+        prev_of_slow->pnext = nullptr;
+
+        Node* prev = nullptr;
+        Node* curr = second_half;
+        Node* next = nullptr;
+
+        // Reverse the second half of the list
+        while (curr != nullptr) {
+            next = curr->pnext;
+            curr->pnext = prev;
+            prev = curr;
+            curr = next;
+        }
+        second_half = prev;
+
+        // Compare the first and second half
+        Node* first_half = phead;
+        while (second_half != nullptr) {
+            if (first_half->data != second_half->data) {
+                cout << "List is not a palindrome." << endl;
+                return false;
+            }
+            first_half = first_half->pnext;
+            second_half = second_half->pnext;
+        }
+        cout << "List is a palindrome." << endl;
+        return true;
+    }
+
+    Node* Search(int x) {
+        Node* temp = phead;
+        while (temp != nullptr) {
+            if (temp->data == x) {
+                cout << "Node with value " << x << " found at address: " << temp << endl;
+                return temp;
+            }
+            temp = temp->pnext;
+        }
+        cout << "Node with value " << x << " not found." << endl;
+        return nullptr;
+    }
+};
+
+void Menu() {
+    LinkedList list;
+    int choice, value;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add Node\n";
+        cout << "2. Print List\n";
+        cout << "3. Find the Middle Node\n";
+        cout << "4. Detect Cycle\n";
+        cout << "5. Reverse List\n";
+        cout << "6. Eliminate Duplicates\n";
+        cout << "7. Check if List is Palindrome\n";
+        cout << "8. Search Node\n";
+        cout << "9. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to add: ";
+                cin >> value;
+                list.AddNode(value);
+                break;
+            case 2:
+                list.PrintList();
+                break;
+            case 3:
+                list.FindMiddleNode();
+                break;
+            case 4:
+                list.DetectCycle();
+                break;
+            case 5:
+                list.ReverseList();
+                break;
+            case 6:
+                list.EliminateDuplicates();
+                break;
+            case 7:
+                list.IsPalindrome();
+                break;
+            case 8:
+                cout << "Enter value to search: ";
+                cin >> value;
+                list.Search(value);
+                break;
+            case 9:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+    } while (choice != 9);
+}
+
+int main() {
+    Menu();
+    return 0;
+}
